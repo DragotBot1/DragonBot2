31db1c958492b38b73e327e74a9151078f848ba0
OchoaBot <Xavy125427@gmail.com>
2021-12-28 16:07:19 -0500

    Prueba xdd

---
+++ src/juegos.js
@@ -0,0 +1,24 @@
+const juegos = (prefix, pushname) => {
+    return `
+*Comandos De Juegos 👾*
+ 
+${prefix}rankgay
+Conoce a el top de los mas gays del grupo
+
+${prefix}gay
+Conoce que tan gay eres 😎
+
+${prefix}cuties
+Prueba tu suerte en el medidor de fan de cuties 😱
+
+${prefix}racista
+Conoce que tan racista eres
+
+_*Pronto Agregare El ttt*_
+     
+_*by Ochoa*_
+`
+
+}
+
+exports.juegos = juegos
---
+++ src/version.js
@@ -0,0 +1,26 @@
+const version = (prefix, pushname) => {
+    return `
+*XavyBot by Ochoa*
+
+*Actualizado:* 24 de Diciembre del 2021
+*Versión actual:* 4.0
+*Ofrecida por:* Ochoa™
+
+
+*INFORME*
+
+FELIS NABIDA, TEAMO
+
+Si no funciona el comando !play o !play2 revisa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+Si vez a alguien que robo mi bot comunicate conmigo con el comando !creador
+
+
+
+_*by Ochoa*_
+`
+
+}
+
+exports.version = version
